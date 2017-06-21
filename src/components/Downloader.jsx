76116502857935ad5191764062cf693e548931ff
rnelson2 <rnelson2@richmond.edu>
2017-06-21 15:46:29 -0400

    added cc-sa license

--- src/components/Downloader.jsx
+++ src/components/Downloader.jsx
@@ -142,7 +142,17 @@ export default class Downloader extends React.Component {
 			</div>
 
 
-				
+			<div
+				style={{
+					textAlign: 'center',
+					margin: '10px auto',
+					clear: 'both'
+				}}
+			>
+				<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">
+					<img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a>
+					<br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
+			</div>
 
 			</div>
 		);
